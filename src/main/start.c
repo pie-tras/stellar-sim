ece24f80640adb322ff5b64cee64797443d9a627
pie-tras <alex.e.pietras@gmail.com>
2021-10-02 00:50:08 -0600

    5 million triangles

--- src/main/start.c
+++ src/main/start.c
@@ -27,15 +27,45 @@ void loadUniforms(vec3 position, vec3 scale, vec3 cameraPos, unsigned int mvp_lo
 int main(void) {
     GLFWwindow* window = createWindow(800, 600, "Test");
 
-	float vertices[] = {
-    	-0.25f, -0.25f, 0.0f, 0.18f, 0.6f, 0.96f,
-     	0.25f, -0.25f, 0.0f, 0.18f, 0.6f, 0.96f,
-    	0.0f,  0.25f, 0.0f, 0.18f, 0.6f, 0.96f,
+    time_t t;
+    srand((unsigned) time(&t));
 
-        0.25f, 0.25f, 0.5f, 1.0f, 0.93f, 0.24f,
-     	0.75f, 0.25f, 0.5f, 1.0f, 0.93f, 0.24f,
-    	0.5f,  0.75f, 0.5f, 1.0f, 0.93f, 0.24f
-	};
+    int width = 175;
+    int height = 175;
+    int depth = 175;
+    int particle_count = width * height * depth;
+
+    double* vertices = malloc(sizeof(double) * 3 * 6 * particle_count);
+
+    int index = 0;
+
+    for (int z = 0; z < depth; z++) {
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                vertices[index] = ((double) (x - 0.5) / width);
+                vertices[index + 1] = ((double) (y - 1.0) / height);
+                vertices[index + 2] = ((double) z / depth);
+                vertices[index + 3] = ((double) rand() / RAND_MAX);
+                vertices[index + 4] = ((double) rand() / RAND_MAX);
+                vertices[index + 5] = ((double) rand() / RAND_MAX);
+
+                vertices[index + 6] = ((double) (x + 0.5) / width);
+                vertices[index + 7] = ((double) (y - 1.0) / height);
+                vertices[index + 8] = ((double) z / depth);
+                vertices[index + 9] = ((double) rand() / RAND_MAX);
+                vertices[index + 10] = ((double) rand() / RAND_MAX);
+                vertices[index + 11] = ((double) rand() / RAND_MAX);
+
+                vertices[index + 12] = ((double) x / width);
+                vertices[index + 13] = ((double) y / height);
+                vertices[index + 14] = ((double) z / depth);
+                vertices[index + 15] = ((double) rand() / RAND_MAX);
+                vertices[index + 16] = ((double) rand() / RAND_MAX);
+                vertices[index + 17] = ((double) rand() / RAND_MAX);
+                index += 18;
+            }
+        }
+    }
 
     unsigned int VAO;
     glGenVertexArrays(1, &VAO);
@@ -45,20 +75,20 @@ int main(void) {
     glBindVertexArray(VAO);
 
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(double) * 18 * particle_count, vertices, GL_STATIC_DRAW);
 
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (const void*)0);
     glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 3, GL_DOUBLE, GL_FALSE, 6 * sizeof(double), (const void*)0);
 
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (const void*)12);
     glEnableVertexAttribArray(1);
+    glVertexAttribPointer(1, 3, GL_DOUBLE, GL_FALSE, 6 * sizeof(double), (const void*)24);
 
 	struct shader* particle_shader = createShader("particle");
 
     startShader(particle_shader);
     glBindVertexArray(VAO);
 
-    vec3 cameraPos = {0, 0, 0};
+    vec3 cameraPos = {-1, -1, -2};
     vec3 position = {0, 0, -2};
     vec3 scale = {2, 2, 2};
 
@@ -74,14 +104,16 @@ int main(void) {
     	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 
         loadUniforms(position, scale, cameraPos, mvp_location);
-        glDrawArrays(GL_TRIANGLES, 0, 6);
+        glDrawArrays(GL_TRIANGLES, 0, particle_count * 3);
 
-        cameraPos[2] -= 0.1f;
-        printf("%f\n", cameraPos[2]);
+        cameraPos[2] += 0.01f;
+      //  printf("%f\n", cameraPos[2]);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
+
+    free(vertices);
 
     cleanShader(particle_shader);
  
